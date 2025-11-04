67689bb1f90b6a03a9e7272a1d969856cacc9cd0
Paulo Otávio <belica.paulo@escola.pr.gov.br>
2025-11-04 09:31:08 -0300

    4/11

---
+++ script.js
@@ -0,0 +1,39 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("clique", mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+const texto = document.querySelector("#entrada-de-texto").value;
+const campoResultado = document.querySelector("#resultado-palavrachave");
+const palavrasChave = processaTexto(texto);
+
+campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+let palavras = texto.split(/\P{L}+/u);
+const frequências = contaFrequências(palavras);
+let ordenadas = Object.keys(frequências).sort(ordenaPalavra);
+
+function ordenaPalavra(p1, p2) {
+return frequências[p2] - frequências[p1];
+}
+
+retornar pedidos.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+let frequencias = {};
+
+for (seja palavra de palavras) {
+if (palavra.trim() === "") continue;
+palavra = palavra.toLowerCase();
+if (frequências[palavra]) {
+frequências[palavra]++;
+} else {
+frequências[palavra] = 1;
+}
+}
+
+frequências de retorno;
+}
