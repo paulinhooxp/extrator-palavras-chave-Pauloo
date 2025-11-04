796c811e326721553dc00085b7b5d5115076c278
Paulo Otávio <belica.paulo@escola.pr.gov.br>
2025-11-04 10:21:14 -0300

    04/11

--- script.js
+++ script.js
@@ -39,5 +39,12 @@ frequências de retorno;
 }
 function tiraPalavrasRuins(palavras){
     const PALAVRAS_RUINS = new Set(["para","nós", "das", "como", "que"]);
-    const palavras 
+    const palavrasBoas = [];
+
+    for (let palavra of palavras){
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2){
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
